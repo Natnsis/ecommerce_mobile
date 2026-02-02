47adc8c5d01f42869f150c3e040e1a547c1f762e
Nati <nsisay49@gmail.com>
2026-02-02 17:37:35 +0300

    reusables init

--- app/index.tsx
+++ app/index.tsx
@@ -1,146 +1,77 @@
-import React from "react";
-import { View, ScrollView } from "react-native";
-import { useRouter } from "expo-router";
-import { Button, ButtonText } from "@/components/ui/button";
-import { Heading } from "@/components/ui/heading";
-import { Text } from "@/components/ui/text";
-import { Card } from "@/components/ui/card";
-import { Image } from "@/components/ui/image";
+import { Button } from '@/components/ui/button';
+import { Icon } from '@/components/ui/icon';
+import { Text } from '@/components/ui/text';
+import { Link, Stack } from 'expo-router';
+import { MoonStarIcon, StarIcon, SunIcon } from 'lucide-react-native';
+import { useColorScheme } from 'nativewind';
+import * as React from 'react';
+import { Image, type ImageStyle, View } from 'react-native';
 
-const Index = () => {
-  const router = useRouter();
-
-  return (
-    <ScrollView className="flex-1 bg-white px-5">
-      <View className="justify-center items-center mt-8">
-        <Image
-          source={require("./../assets/images/assosa.jpg")}
-          alt="Welcome Image"
-          className="w-full h-52 mb-6 rounded-lg shadow-md"
-          resizeMode="cover"
-        />
-      </View>
-      <Heading size="2xl" className="text-center w-full">
-        Welcome to Assosa E-Commerce
-      </Heading>
-      <Text size="sm" className="text-center">
-        Discover and shop for your favorite products from the comfort of your
-        home.
-      </Text>
-      <Card variant="outline" className="mt-5">
-        <Text size="lg">Why Shop With Us?</Text>
-        <Text size="md">- Wide variety of products to choose from.</Text>
-        <Text size="md">- Affordable prices and great deals.</Text>
-        <Text size="md">- Fast and reliable delivery services.</Text>
-      </Card>
-
-      <View className="mt-8">
-        <Text size="xl">Featured Categories</Text>
-        <View className="gap-5">
-          <Card size="md" variant="filled" className="flex-row gap-1 w-full">
-            <View className="w-1/3 bg-white p-2 rounded-md">
-              <Image
-                size="md"
-                source={require("./../assets/images/cloths.jpg")}
-                alt="image"
-                resizeMode="cover"
-                className="w-full"
-              />
-              <Heading size="sm" className="text-center">
-                Cloths
-              </Heading>
-            </View>
-            <View className="w-1/3 bg-white p-2 rounded-md">
-              <Image
-                size="md"
-                source={require("./../assets/images/Accessories.jpg")}
-                alt="image"
-                resizeMode="cover"
-                className="w-full"
-              />
-              <Heading size="sm" className="text-center">
-                Accessorie
-              </Heading>
-            </View>
-            <View className="w-1/3 bg-white p-2 rounded-md">
-              <Image
-                size="md"
-                source={require("./../assets/images/detergentsImg.jpg")}
-                alt="image"
-                resizeMode="cover"
-                className="w-full"
-              />
-              <Heading size="sm" className="text-center">
-                Detergents
-              </Heading>
-            </View>
-          </Card>
-
-          <Card size="md" variant="filled" className="flex-row gap-1 w-full">
-            <View className="w-1/3 bg-white p-2 rounded-md">
-              <Image
-                size="md"
-                source={require("./../assets/images/electronicss.jpg")}
-                alt="image"
-                resizeMode="cover"
-                className="w-full"
-              />
-              <Heading size="sm" className="text-center">
-                Electronics
-              </Heading>
-            </View>
-            <View className="w-1/3 bg-white p-2 rounded-md">
-              <Image
-                size="md"
-                source={require("./../assets/images/foodImg.jpg")}
-                alt="image"
-                resizeMode="cover"
-                className="w-full"
-              />
-              <Heading size="sm" className="text-center">
-                Food
-              </Heading>
-            </View>
-            <View className="w-1/3 bg-white p-2 rounded-md">
-              <Image
-                size="md"
-                source={require("./../assets/images/luxurys.jpg")}
-                alt="image"
-                resizeMode="cover"
-                className="w-full"
-              />
-              <Heading size="sm" className="text-center">
-                Luxury
-              </Heading>
-            </View>
-          </Card>
-
-          <Card size="md" variant="filled" className="flex-row gap-1 w-full">
-            <View className="w-1/3 bg-white p-2 rounded-md">
-              <Image
-                size="md"
-                source={require("./../assets/images/toolss.jpg")}
-                alt="image"
-                resizeMode="cover"
-                className="w-full"
-              />
-              <Heading size="sm" className="text-center">
-                Tools
-              </Heading>
-            </View>
-          </Card>
-        </View>
-      </View>
-
-      <Button
-        onPress={() => router.push("/login")}
-        variant="solid"
-        className="mt-12 w-full mb-5"
-      >
-        <ButtonText>Get Started</ButtonText>
-      </Button>
-    </ScrollView>
-  );
+const LOGO = {
+  light: require('@/assets/images/react-native-reusables-light.png'),
+  dark: require('@/assets/images/react-native-reusables-dark.png'),
 };
 
-export default Index;
+const SCREEN_OPTIONS = {
+  title: 'React Native Reusables',
+  headerTransparent: true,
+  headerRight: () => <ThemeToggle />,
+};
+
+const IMAGE_STYLE: ImageStyle = {
+  height: 76,
+  width: 76,
+};
+
+export default function Screen() {
+  const { colorScheme } = useColorScheme();
+
+  return (
+    <>
+      <Stack.Screen options={SCREEN_OPTIONS} />
+      <View className="flex-1 items-center justify-center gap-8 p-4">
+        <Image source={LOGO[colorScheme ?? 'light']} style={IMAGE_STYLE} resizeMode="contain" />
+        <View className="gap-2 p-4">
+          <Text className="ios:text-foreground font-mono text-sm text-muted-foreground">
+            1. Edit <Text variant="code">app/index.tsx</Text> to get started.
+          </Text>
+          <Text className="ios:text-foreground font-mono text-sm text-muted-foreground">
+            2. Save to see your changes instantly.
+          </Text>
+        </View>
+        <View className="flex-row gap-2">
+          <Link href="https://reactnativereusables.com" asChild>
+            <Button>
+              <Text>Browse the Docs</Text>
+            </Button>
+          </Link>
+          <Link href="https://github.com/founded-labs/react-native-reusables" asChild>
+            <Button variant="ghost">
+              <Text>Star the Repo</Text>
+              <Icon as={StarIcon} />
+            </Button>
+          </Link>
+        </View>
+      </View>
+    </>
+  );
+}
+
+const THEME_ICONS = {
+  light: SunIcon,
+  dark: MoonStarIcon,
+};
+
+function ThemeToggle() {
+  const { colorScheme, toggleColorScheme } = useColorScheme();
+
+  return (
+    <Button
+      onPressIn={toggleColorScheme}
+      size="icon"
+      variant="ghost"
+      className="ios:size-9 rounded-full web:mx-4">
+      <Icon as={THEME_ICONS[colorScheme ?? 'light']} className="size-5" />
+    </Button>
+  );
+}
